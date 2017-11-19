5ad1a71b6be54e595bbbbaec1ebae75cc8f7786b
Steven Fitzpatrick <steven.fitzpatrick1985@gmail.com>
2017-11-19 01:18:54 +0000

    1.0.0

---
+++ src/Input.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Input extends Component {
+    render() {
+        return (
+            <div>
+                <input />
+            </div>
+        );
+    }
+}
+
+export default Input;
