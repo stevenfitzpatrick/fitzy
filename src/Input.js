e5eaae94fb69fb2c224eaa3145c02e752ad241f9
Steven Fitzpatrick <steven.fitzpatrick1985@gmail.com>
2017-11-26 21:49:30 +0000

    1.0.1

--- src/Input.js
+++ src/Input.js
@@ -4,7 +4,7 @@ class Input extends Component {
     render() {
         return (
             <div>
-                <input />
+                <input type="text" />
             </div>
         );
     }
