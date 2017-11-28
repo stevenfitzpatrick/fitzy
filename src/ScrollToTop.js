9e8a49b2450aed4190b3d0633461df14e0120b00
Steven Fitzpatrick <steven.fitzpatrick1985@gmail.com>
2017-11-28 22:22:12 +0000

    Add Typings

---
+++ src/ScrollToTop.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ScrollToTop extends Component {
+    render() {
+        return <div />;
+    }
+}
+
+export default ScrollToTop;
---
+++ src/fitzy.d.ts
@@ -0,0 +1,10 @@
+declare module 'fitzy' {
+    import * as React from 'react';
+
+    export interface DraggableCoreProps {}
+    export interface ScrollToTopProps {}
+
+    export class Input extends React.Component<InputProps, any> {}
+
+    export class ScrollToTop extends React.Component<ScrollToTopProps, any> {}
+}
