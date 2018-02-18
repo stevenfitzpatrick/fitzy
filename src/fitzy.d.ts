ca6ad8197cf3ef2747ee9d69b4df184ced84668f
Steven Fitzpatrick <steven.fitzpatrick1985@gmail.com>
2018-02-18 12:58:32 +0000

    1.0.19

--- src/fitzy.d.ts
+++ src/fitzy.d.ts
@@ -1,7 +1,9 @@
 declare module 'fitzy' {
   import * as React from 'react';
 
-  export interface DraggableCoreProps {}
+  export interface InputProps {}
+  export interface DropdownProps {}
 
   export class Input extends React.Component<InputProps, any> {}
+  export class Dropdown extends React.Component<DropdownProps, any> {}
 }
--- src/fitzy.js
+++ src/fitzy.js
@@ -1,7 +1,7 @@
 // Forms
 export { default as Input } from './components/Input/Input';
+export { default as Dropdown } from './components/Dropdown/Dropdown';
 export { default as RadioGroup } from './components/RadioGroup/RadioGroup';
 export { default as RadioOption } from './components/RadioGroup/RadioOption';
 
 // Components
-
