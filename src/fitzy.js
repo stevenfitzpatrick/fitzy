90a77c4e3f1eb8223308b5282b8bb4a1631e06ca
Steven Fitzpatrick <steven.fitzpatrick1985@gmail.com>
2018-02-26 23:38:34 +0000

    fix(Button): Add default export for button

--- src/fitzy.js
+++ src/fitzy.js
@@ -1,5 +1,6 @@
 // Forms
 export { default as Input } from './components/Input/Input';
+export { default as Button } from './components/Button/Button';
 export { default as Dropdown } from './components/Dropdown/Dropdown';
 export { default as RadioGroup } from './components/RadioGroup/RadioGroup';
 export { default as RadioOption } from './components/RadioGroup/RadioOption';
