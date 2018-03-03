7bc8fe587f21b31af9107a7410a89997c982028f
Steven Fitzpatrick <steven.fitzpatrick1985@gmail.com>
2018-03-03 00:17:48 +0000

    fix(Alert): Forgot to add to export

--- src/fitzy.js
+++ src/fitzy.js
@@ -6,3 +6,4 @@ export { default as RadioGroup } from './components/RadioGroup/RadioGroup';
 export { default as RadioOption } from './components/RadioGroup/RadioOption';
 
 // Components
+export { default as Alert } from './components/Alert/Alert';
